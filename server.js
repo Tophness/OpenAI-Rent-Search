72289181469cb888772b4622905f1581f6d616c8
Chris Malone <chrismrulz@hotmail.com>
2023-05-30 23:00:45 +1000

    res.set

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.use(proxy('https://www.rent.com.au', {
   },
   userResDecorator: function(proxyRes, proxyResData, req, res) {
     if (req.url.indexOf('/properties') !== -1) {
-       res.setHeaders("content-type", "application/json; charset=utf-8");
-       res.setHeaders("accept", "application/json");
+       res.set("content-type", "application/json; charset=utf-8");
+       res.set("accept", "application/json");
        return JSON.stringify(extractListingDetails(proxyResData));
     }
     else{
