f4b38adf8dc627a88de2800d86dc9e6971fd8cb5
Chris Malone <chrismrulz@hotmail.com>
2023-05-31 00:54:38 +1000

    add url

--- server.js
+++ server.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
+const cheerio = require('cheerio');
+
+function extractListingDetails(html) {
+  const $ = cheerio.load(html);
+  const listings = [];
+
+  $('article.property-cell').each((index, element) => {
+    const address = $(element).find('h2.address').text().trim();
+    const imageUrl = $(element).find('img.card-photo').attr('src');
+    const priceElement = $(element).find('span.price');
+    const propType = $(priceElement).find('.property-type').text().trim();
+    priceElement.find('.property-type').remove();
+    const price = priceElement.text().trim();
+    const ldJsonScripts = $(element).find('script[type="application/ld+json"]').first();
+    const features = [];
+    let description = "";
+    let url = "";
+
+    $(element).find('ul.features li.feature').each((index, element) => {
+      const value = $(element).find('span.value').text().trim();
+      features.push(value);
+    });
+
+    if (ldJsonScripts.length > 0) {
+      let jsonText = ldJsonScripts.html();
+      jsonText = jsonText.replace('//<![CDATA[', '').replace('//]]>', '');
+      try {
+        const json = JSON.parse(jsonText);
+        const isEmpty = Object.keys(json).length === 0;
+
+        if (!isEmpty && json['@type'] !== "RentAction") {
+          description = json.description;
+          url = json.url;
+        }
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+      }
+    }
+
+    listings.push({
+      address: address,
+      imageUrl: imageUrl,
+      price: price,
+      features: features,
+      propType: propType,
+      description: description,
+      url: url
+    });
+  });
+
+  return listings;
+}
 
 const app = express();
 app.use(express.static('public'));
-app.use(proxy('https://www.domain.com.au', {
+app.use(proxy('https://www.rent.com.au', {
   proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
-    if (srcReq.url.indexOf('/rent') !== -1) {
-       proxyReqOpts.headers["content-type"] = "application/json; charset=utf-8";
-       proxyReqOpts.headers["accept"] = "application/json";
+    if (srcReq.url.indexOf('/properties') !== -1) {
+      proxyReqOpts.headers["Accept"] = "text/html";
     }
+    proxyReqOpts.headers["Cookie"] = "";
     proxyReqOpts.headers["Access-Control-Allow-Origin"] = "*";
     proxyReqOpts.headers["Access-Control-Allow-Methods"] = "*";
     proxyReqOpts.headers["Access-Control-Allow-Headers"] = "*";
@@ -16,72 +68,42 @@ app.use(proxy('https://www.domain.com.au', {
     return proxyReqOpts;
   },
   userResDecorator: function(proxyRes, proxyResData, req, res) {
-    if (req.url.indexOf('/rent') !== -1) {
-      const data = JSON.parse(proxyResData.toString('utf8'));
-      if (data.props && data.props.listingsMap) {
-        let trimmedData = data.props.listingsMap;
-        const numKeys = Object.keys(trimmedData);
-        const numObjects = numKeys.length;
-          if (numObjects > 50) {
-          for (let i = 50; i < numObjects; i++) {
-            delete trimmedData[numKeys[i]];
-          }
-        }
-        for (const id in trimmedData) {
-         if (trimmedData.hasOwnProperty(id)) {
-           delete trimmedData[id].id;
-           delete trimmedData[id].listingType;
-           delete trimmedData[id].listingModel.skeletonImages;
-           delete trimmedData[id].listingModel.auction;
-           delete trimmedData[id].listingModel.branding;
-           delete trimmedData[id].listingModel.brandingAppearance;
-           delete trimmedData[id].listingModel.displaySearchPriceRange;
-           delete trimmedData[id].listingModel.enableSingleLineAddress;
-           delete trimmedData[id].listingModel.hasVideo;
-           delete trimmedData[id].listingModel.promoType;
-           delete trimmedData[id].listingModel.tags;
-         }
-        }
-        const imgParam = req.url.match('[?&]images=([^&]+)');
-        if (imgParam) {
-          const numImages = parseInt(imgParam[1]);
-          if(numImages > 0){
-            for (let key in trimmedData) {
-              if (trimmedData.hasOwnProperty(key)) {
-                const images = trimmedData[key].listingModel.images;
-                if (Array.isArray(images) && images.length > numImages) {
-                  trimmedData[key].listingModel.images = images.slice(0, numImages);
-                }
-              }
-            }
-          }
-          else{
-            for (let key in trimmedData) {
-              if (trimmedData.hasOwnProperty(key)) {
-                delete trimmedData[key].listingModel.images;
-              }
-            }
-          }
-        }
-        else{
-            for (let key in trimmedData) {
-              if (trimmedData.hasOwnProperty(key)) {
-                delete trimmedData[key].listingModel.images;
-              }
-            }
-        }
-        trimmedData = {
-          'totalPages': data.props.pageViewMetadata.searchResponse.SearchResults.totalPages,
-          'page': data.props.pageViewMetadata.searchResponse.SearchResults.page,
-          'totalResults': data.props.pageViewMetadata.searchResponse.SearchResults.totalResults,
-          'resultsPerPage': data.props.listingSearchResultIds.length,
-          'listings': trimmedData
-        };
-        return JSON.stringify(trimmedData);
-      }
-      else{
-         return proxyResData;
-      }
+    res.set("Access-Control-Allow-Origin","*");
+    res.set("Access-Control-Allow-Methods","*");
+    res.set("Access-Control-Allow-Headers","*");
+    res.set("Access-Control-Allow-Credentials","true");
+    res.set("x-amz-apigw-id","");
+    res.set("x-amzn-remapped-connection","");
+    res.set("x-amz-apigw-id","");
+    res.set("x-amzn-remapped-date","");
+    res.set("x-amzn-remapped-server","");
+    res.set("x-amzn-requestid","");
+    res.set("x-amz-apigw-id","");
+    res.set("x-content-type-options","");
+    res.set("x-frame-options","");
+    res.set("x-amz-apigw-id","");
+    res.set("x-permitted-cross-domain-policies","*");
+    res.set("x-amz-apigw-id","");
+    res.set("x-powered-by","");
+    res.set("x-render-origin-server","");
+    res.set("x-request-id","");
+    res.set("x-xss-protection","");
+    res.set("referrer-policy","");
+    res.set("link","");
+    res.set("etag","");
+
+    if (req.url.indexOf('/properties') !== -1) {
+       res.set("content-type", "application/json; charset=utf-8");
+       res.set("accept", "application/json");
+       const imgParam = req.url.match('[?&]images=([^&]+)');
+       let returnJSON = extractListingDetails(proxyResData);
+       if (!imgParam || imgParam[1] == "0") {
+         returnJSON = returnJSON.map(obj => {
+           delete obj.imageUrl;
+           return obj;
+         });
+       }
+       return JSON.stringify(returnJSON);
     }
     else{
        return proxyResData;
